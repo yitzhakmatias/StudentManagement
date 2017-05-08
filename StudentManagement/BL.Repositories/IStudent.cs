2582494ac83547273abb1c23097fec14f7e2149e
yitzhak matias <matias6@gmail.com>
2017-05-08 17:21:42 -0400

    Basic impl

---
+++ StudentManagement/BL.Repositories/IStudent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BL.Models;
+
+namespace BL.Repositories
+{
+    public interface IStudent
+    {
+        List<Student> ReafCsvFile(string filePath);
+
+
+    }
+}
